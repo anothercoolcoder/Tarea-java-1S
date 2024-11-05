841014d49c97f8ff7b413620e2e66ea44615c38b
Andres Nino <andresalejandroninoaraujo@gmail.com>
2024-11-05 18:50:14 -0500

    modificacion hasta 24

--- ejercicio17.java
+++ ejercicio17.java
@@ -2,25 +2,23 @@ import java.util.Scanner;
 
 public class ejercicio17 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Ingresa el primer número:");
-        int num1 = scanner.nextInt();
-        
-        if (num1 > 7){
-            System.out.println("Excelente");
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Ingresa el primer número:");
+            int num1 = scanner.nextInt();
+            
+            if (num1 > 7){
+                System.out.println("Excelente");
+            }
+            else
+                if (num1 < 7 && num1 > 5) {
+                    System.out.println("Bueno");
+                }
+                else
+                    if (num1 < 5 && num1 > 3) {
+                        System.out.println("Regular");
+                    }
+                    else
+                        System.out.println("Malo");
         }
-        else
-        if (num1 < 7 && num1 > 5) {
-            System.out.println("Bueno");
-        }
-        else
-        if (num1 < 5 && num1 > 3) {
-            System.out.println("Regular");
-        }
-        else
-        System.out.println("Malo");
-
-        scanner.close();
     }
 }
--- ejercicio22.java
+++ ejercicio22.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 public class ejercicio22 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese un valor para N");
-        int n = scanner.nextInt();
-        for(int i = 0; i < n + 1; i++)
-        if (i== 1)
-        System.out.println("Primera iteracion (Hola mundo)");
-        else
-        System.out.println("Iteracion, " + i);
-        scanner.close();
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Ingrese un valor para N");
+            int n = scanner.nextInt();
+            for(int i = 0; i < n + 1; i++)
+                if (i== 1)
+                    System.out.println("Primera iteracion (Hola mundo)");
+                else
+                    System.out.println("Iteracion, " + i);
+        }
     }
 }
