9b50eaa080dd9b03a8972d4337c17d00aca0d18d
Andres Nino <andresalejandroninoaraujo@gmail.com>
2024-11-07 20:27:04 -0500

    aaaaaaaaaaaaaaaaa

---
+++ 20-40/Main.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        while (sc.hasNextLine()) {
+            String secuencia = sc.nextLine();
+            if (secuencia.isEmpty()) break;
+            
+            int puntosA = 0, puntosB = 0;
+            int juegosA = 0, juegosB = 0;
+            boolean servicioA = true;
+            
+            for (int i = 0; i < secuencia.length(); i++) {
+                char punto = secuencia.charAt(i);
+                
+                if (punto == 'S') {
+                    if (servicioA) puntosA++;
+                    else puntosB++;
+                } else if (punto == 'R') {
+                    if (servicioA) puntosB++;
+                    else puntosA++;
+                }
+                
+                if (puntosA >= 4 && puntosA >= puntosB + 2) {
+                    juegosA++;
+                    puntosA = 0;
+                    puntosB = 0;
+                    servicioA = !servicioA;
+                    
+                    if (juegosA >= 6 && juegosA >= juegosB + 2) {
+                        break;
+                    }
+                } else if (puntosB >= 4 && puntosB >= puntosA + 2) {
+                    juegosB++;
+                    puntosA = 0;
+                    puntosB = 0;
+                    servicioA = !servicioA;
+                    
+                    if (juegosB >= 6 && juegosB >= juegosA + 2) {
+                        break;
+                    }
+                }
+            }
+            
+            System.out.println(juegosA + "-" + juegosB);
+        }
+        
+        sc.close();
+    }
+}
---
+++ 20-40/ejercicio35.java
@@ -0,0 +1,5 @@
+public class ejercicio35 {
+    public static void main(String[] args) {
+        
+    }    
+}
